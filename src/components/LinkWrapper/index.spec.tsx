d2c8008101c8b114ad6ca40da0a8eb453b097e16
Breno Xavier <brenoxavier8.bx@gmail.com>
2022-03-14 07:22:11 -0300

    test: ensure LinkWrapper render link with the correct href

--- src/components/LinkWrapper/index.spec.tsx
+++ src/components/LinkWrapper/index.spec.tsx
@@ -7,4 +7,11 @@ describe('<LinkWrapper /> component', () => {
     const childrenExpected = screen.getByText('Any Children')
     expect(childrenExpected).toBeInTheDocument()
   })
+
+  it('Should render the link correctly', () => {
+    const myLink = '/any-link'
+    render(<LinkWrapper href={myLink}>Any link</LinkWrapper>)
+    const LinkExpected = screen.getByText('Any link')
+    expect(LinkExpected).toHaveAttribute('href', myLink)
+  })
 })
