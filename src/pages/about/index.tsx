018ef459ff4918fbb4cd7cfcba7ae0504f70d507
Breno Xavier <brenoxavier8.bx@gmail.com>
2022-03-14 22:06:16 -0300

    feat: add server side call for cms using graphql

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,5 +1,16 @@
 import AboutTemplate from 'templates/About'
+import { GET_PAGES } from 'graphql/queries'
+import client from 'graphql/client'
 
 const About = () => <AboutTemplate />
 
 export default About
+
+export const getStaticProps = async () => {
+  const { pages } = await client.request(GET_PAGES)
+  console.log(pages)
+
+  return {
+    props: {}
+  }
+}
--- src/templates/About/index.tsx
+++ src/templates/About/index.tsx
@@ -1,12 +1,17 @@
+import dynamic from 'next/dynamic'
 import { CloseOutline } from '@styled-icons/evaicons-outline'
 import { LinkWrapper } from 'components'
 import * as S from './styles'
 
+const DynamicLinkWrapper = dynamic(() => import('components/LinkWrapper'), {
+  ssr: false
+})
+
 const AboutTemplate = () => (
   <S.Content>
-    <LinkWrapper href="/">
+    <DynamicLinkWrapper href="/">
       <CloseOutline size={32} />
-    </LinkWrapper>
+    </DynamicLinkWrapper>
     <S.Heading>My Trips</S.Heading>
     <S.Body>
       <p>
