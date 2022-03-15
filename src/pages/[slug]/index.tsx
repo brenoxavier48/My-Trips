b37fc8be114ce355c363ad6a03b53a8614f1ac2b
Breno Xavier <brenoxavier8.bx@gmail.com>
2022-03-15 08:33:19 -0300

    refactor: transform about page in a generic page

---
+++ src/pages/[slug]/index.tsx
@@ -0,0 +1,42 @@
+import PagesTemplate, { PageTemplatePropTypes } from 'templates/Pages'
+import { GET_PAGES, GET_PAGE_BY_SLUG } from 'graphql/queries'
+import { GetPageResult, GetPageBySlugResult } from 'graphql/types'
+import client from 'graphql/client'
+import { useRouter } from 'next/router'
+import { GetStaticProps } from 'next'
+
+const Page: React.FC<PageTemplatePropTypes> = ({ heading, body }) => {
+  const router = useRouter()
+
+  if (router.isFallback) return null
+
+  return <PagesTemplate heading={heading} body={body} />
+}
+
+export default Page
+
+export const getStaticPaths = async () => {
+  const { pages } = await client.request<GetPageResult>(GET_PAGES, { first: 3 })
+
+  const paths = pages.map(({ slug }) => ({ params: { slug } }))
+
+  return {
+    paths,
+    fallback: true
+  }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { page } = await client.request<GetPageBySlugResult>(GET_PAGE_BY_SLUG, {
+    slug: `${params?.slug}`
+  })
+
+  if (page === null) return { notFound: true }
+
+  return {
+    props: {
+      body: page.body,
+      heading: page.heading
+    }
+  }
+}
--- src/templates/Pages/index.tsx
+++ src/templates/Pages/index.tsx
@@ -1,24 +1,24 @@
 import dynamic from 'next/dynamic'
 import { CloseOutline } from '@styled-icons/evaicons-outline'
-import { LinkWrapper } from 'components'
 import * as S from './styles'
 
 const DynamicLinkWrapper = dynamic(() => import('components/LinkWrapper'), {
   ssr: false
 })
 
-const PagesTemplate = () => (
+export type PageTemplatePropTypes = {
+  heading: string
+  body: string
+}
+
+const PagesTemplate: React.FC<PageTemplatePropTypes> = ({ heading, body }) => (
   <S.Content>
     <DynamicLinkWrapper href="/">
       <CloseOutline size={32} />
     </DynamicLinkWrapper>
-    <S.Heading>My Trips</S.Heading>
+    <S.Heading>{heading}</S.Heading>
     <S.Body>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
-        repellendus quo iste architecto, animi labore nam vero error eveniet
-        doloribus quam, quas cum nobis? Ducimus qui officia eligendi omnis ab!
-      </p>
+      <div dangerouslySetInnerHTML={{ __html: body }} />
     </S.Body>
   </S.Content>
 )
