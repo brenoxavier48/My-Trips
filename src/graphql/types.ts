a3202b493f4e20dd449350701c31a84e05cffe66
Breno Xavier <brenoxavier8.bx@gmail.com>
2022-03-15 22:14:52 -0300

    feat: add query for get places

--- src/graphql/types.ts
+++ src/graphql/types.ts
@@ -1,15 +1,22 @@
-export type Page = {
-  id: string
-  heading: string
-  slug: string
-  body: {
-    html: string
-  }
-}
+import { Place, Page } from 'domain/abstractions'
+
 export type GetPageResult = {
   pages: Page[]
 }
 
 export type GetPageBySlugResult = {
   page: Page
+}
+
+export type GetPlacesResult = {
+  places: Place & {
+    description: {
+      html: string
+    }
+    gallery: {
+      url: string
+      height: number
+      width: number
+    }
+  }
 }
