fa44790de37156d6dfc7033e73959e34801827c6
Breno Xavier <brenoxavier8.bx@gmail.com>
2022-03-16 21:18:05 -0300

    feat: add SEO

---
+++ next-seo.config.ts
@@ -0,0 +1,13 @@
+export const SEO = {
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: 'https://www.my-trips.com/',
+    site_name: 'My Trisps'
+  },
+  twitter: {
+    handle: '@brenobhann',
+    site: '@site',
+    cardType: 'summary_large_image'
+  }
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,11 +4,13 @@ import NextNProgress from 'nextjs-progressbar'
 
 import GlobalStyles from 'styles/global'
 
+import { DefaultSeo } from 'next-seo'
+import { SEO } from '../../next-seo.config'
+
 function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>My Trips</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link
@@ -17,11 +19,8 @@ function App({ Component, pageProps }: AppProps) {
           integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
           crossOrigin=""
         />
-        <meta
-          name="description"
-          content="A simple project to show my favorite spots in the world."
-        />
       </Head>
+      <DefaultSeo {...SEO} />
       <GlobalStyles />
       <NextNProgress color="#29d" options={{ trickleSpeed: 50 }} />
       <Component {...pageProps} />
