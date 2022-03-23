2a7aa2afd9ba72b0eda10c27547f5926a021fe58
Breno Xavier <brenoxavier8.bx@gmail.com>
2022-03-23 19:59:22 -0300

    feat: change styles of the map

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { MapContainer, TileLayer, Marker } from 'react-leaflet'
 import { Place } from 'domain/abstractions'
 import { useRouter } from 'next/router'
+import { MapWrapper } from './styles'
 
 export type MapPropTypes = {
   places?: Place[]
@@ -10,29 +11,32 @@ export type MapPropTypes = {
 const Map: React.FC<MapPropTypes> = ({ places }) => {
   const { push } = useRouter()
   return (
-    <MapContainer
-      center={[51.505, -0.09]}
-      zoom={3}
-      style={{ height: '100%', width: '100%' }}
-    >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      {places?.map(({ id, name, location, slug }) => {
-        const { latitude, longitude } = location
-        return (
-          <Marker
-            position={[latitude, longitude]}
-            title={name}
-            key={`place-${name}-${id}`}
-            eventHandlers={{
-              click: () => push(`/place/${slug}`)
-            }}
-          />
-        )
-      })}
-    </MapContainer>
+    <MapWrapper>
+      <MapContainer
+        center={[51.505, -0.09]}
+        zoom={3}
+        minZoom={3}
+        style={{ height: '100%', width: '100%' }}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {places?.map(({ id, name, location, slug }) => {
+          const { latitude, longitude } = location
+          return (
+            <Marker
+              position={[latitude, longitude]}
+              title={name}
+              key={`place-${name}-${id}`}
+              eventHandlers={{
+                click: () => push(`/place/${slug}`)
+              }}
+            />
+          )
+        })}
+      </MapContainer>
+    </MapWrapper>
   )
 }
 
